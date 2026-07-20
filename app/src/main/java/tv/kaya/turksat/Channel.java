84cfb75812427a3908a7d0923c4dc87fbee2a67e
KayaJR356 <kayakayaforza@outlook.com>
2026-07-20 22:16:38 +0300

    app/src/main/java/tv/kaya/turksat/Channel.java ekle

---
+++ app/src/main/java/tv/kaya/turksat/Channel.java
@@ -0,0 +1,6 @@
+package tv.kaya.turksat;
+
+final class Channel {
+    final int number; final String name; final String url;
+    Channel(int number, String name, String url) { this.number=number; this.name=name; this.url=url; }
+}
